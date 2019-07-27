cc6d25d8fbe05845e49935847531ca0fe0af6c45
Ajay Pathak <APathak2@slb.com>
2019-07-27 15:47:25 +0530

    Created Solution For Azure Service Bus

---
+++ AzureServiceBus/AzureServiceBus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureServiceBus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
