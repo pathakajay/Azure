89b5b1c2ad05ebf346f361fb59cc83b1f00d926e
Ajay Pathak <APathak2@slb.com>
2019-07-27 15:29:32 +0530

    Added Basic Queue Operations

--- AzureStorageQueue/AzureStorageQueue/StorageQueueSample.cs
+++ AzureStorageQueue/AzureStorageQueue/StorageQueueSample.cs
@@ -28,26 +28,68 @@ namespace AzureStorageQueue
 
             await CreateQueue();
 
-            var queueClient = GetQueueClient(connectionString);
-            CloudQueue queue = queueClient.GetQueueReference(QueueName);
-            await queue.AddMessageAsync(new CloudQueueMessage("Hello World"));
-
-
+            await BasicQueueOperation(connectionString);
+            await UpdateQueueMessage(connectionString);
+            await DequeueQueue(connectionString);
 
             Console.WriteLine("Press a key to exit.");
             Console.ReadLine();
         }
 
+        private static async Task BasicQueueOperation(string connectionString)
+        {
+            var queueClient = GetQueueClient(connectionString);
+            CloudQueue queue = queueClient.GetQueueReference(QueueName);
+            for (int i = 0; i < 5; i++)
+            {
+                await queue.AddMessageAsync(new CloudQueueMessage("Hello World" + "\t" + i.ToString()));
+            }
+            Console.WriteLine("Peek at the next message");
+            CloudQueueMessage queueMessage = await queue.PeekMessageAsync();
+            if (queueMessage != null)
+            {
+                Console.WriteLine("The peeked message is: {0}", queueMessage.AsString);
+            }
+
+            Console.WriteLine("De-queue the next message");
+            CloudQueueMessage cloudQueueMessage = await queue.GetMessageAsync();
+            if (cloudQueueMessage != null)
+            {
+                Console.WriteLine("Processing & deleting message with content: {0}", cloudQueueMessage.AsString);
+                await queue.DeleteMessageAsync(cloudQueueMessage);
+            }
+        }
+
+        private static async Task UpdateQueueMessage(string connectionString)
+        {
+            var queueClient = GetQueueClient(connectionString);
+            CloudQueue queue = queueClient.GetQueueReference(QueueName);
+
+            Console.WriteLine("De-queue the next message");
+            CloudQueueMessage cloudQueueMessage = await queue.GetMessageAsync();
+
+            if (cloudQueueMessage != null)
+            {
+                cloudQueueMessage.SetMessageContent("Content Updated");
+                await queue.UpdateMessageAsync(cloudQueueMessage, TimeSpan.Zero,
+                    MessageUpdateFields.Content | MessageUpdateFields.Visibility);
+
+
+            }
+        }
 
         public static async Task<bool> CreateQueue()
         {
             string name = "demotest-" + Guid.NewGuid().ToString();
             var connectionString = new StorageQueueSample()._appOptions.ConnectionString;
             var client = GetQueueClient(connectionString);
+
+            // Retrieve a reference to a queue.
             CloudQueue queue = client.GetQueueReference(name);
 
             try
             {
+                // Create the queue if it doesn't already exist.
                 await queue.CreateIfNotExistsAsync();
             }
             catch (Exception e)
@@ -59,10 +101,41 @@ namespace AzureStorageQueue
 
         }
 
+
+        public static async Task DequeueQueue(string connectionString)
+        {
+            var queueClient = GetQueueClient(connectionString);
+            CloudQueue queue = queueClient.GetQueueReference(QueueName);
+
+            Console.WriteLine("Get the queue length");
+
+            await queue.FetchAttributesAsync();
+            int? cachedMessageCount = queue.ApproximateMessageCount;
+            Console.WriteLine("Number of messages in queue: {0}", cachedMessageCount);
+            QueueRequestOptions options = new QueueRequestOptions();
+            OperationContext operationContext = new OperationContext();
+
+            if (cachedMessageCount == null)
+            {
+                Console.WriteLine("No Message Present in Queue");
+                return;
+            }
+
+            foreach (CloudQueueMessage message in await queue.GetMessagesAsync(cachedMessageCount.Value,
+                new TimeSpan(0, 0, 5, 0), options, operationContext))
+            {
+                // Process all messages in less than 5 minutes, deleting each message after processing.
+
+                Console.WriteLine("Processing {0} Message", message.AsString);
+                await queue.DeleteMessageAsync(message);
+            }
+        }
         public static CloudQueueClient GetQueueClient(string connectionString)
         {
-
+            // Retrieve storage account from connection string.
             CloudStorageAccount account = CloudStorageAccount.Parse(connectionString);
+
+            // Create the queue client.
             CloudQueueClient client = account.CreateCloudQueueClient();
             return client;
         }
