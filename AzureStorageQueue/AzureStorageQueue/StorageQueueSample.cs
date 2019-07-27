e49c9cf0baba6aa6ca6c77d394c7020da6bf260a
Ajay Pathak <APathak2@slb.com>
2019-07-27 10:47:32 +0530

    implemented Create Queue Method

--- AzureStorageQueue/AzureStorageQueue/StorageQueueSample.cs
+++ AzureStorageQueue/AzureStorageQueue/StorageQueueSample.cs
@@ -1,32 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace AzureStorageQueue
 {
-   public class StorageQueueSample
+    public class StorageQueueSample
     {
-        private   readonly IConfigurationRoot settingsCache;
-        private   AppOptions appOptions;
+        private readonly AppOptions _appOptions;
+/*
+        private const string QueueName = "samplequeue";
+*/
 
         public StorageQueueSample()
         {
             var builder = new ConfigurationBuilder();
             builder.AddJsonFile("appsettings.json", true, true);
 
-            settingsCache = builder.Build();
-            appOptions = new AppOptions();
-            settingsCache.Bind(appOptions);
+            var settingsCache = builder.Build();
+            _appOptions = new AppOptions();
+            settingsCache.Bind(_appOptions);
         }
         public static async Task Run()
         {
-            StorageQueueSample queueSample= new StorageQueueSample();
 
-            
+
+            await CreateQueue();
+
+
             Console.WriteLine("Press a key to exit.");
             Console.ReadLine();
+        }
+
+
+        public static async Task<bool> CreateQueue()
+        {
+            string name = "demotest-" + Guid.NewGuid().ToString();
+            var connectionString = new StorageQueueSample()._appOptions.ConnectionString;
+            var client = GetQueueClient(connectionString);
+            CloudQueue queue = client.GetQueueReference(name);
+
+            try
+            {
+                await queue.CreateIfNotExistsAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return true;
+
+        }
+
+        public static CloudQueueClient GetQueueClient(string connectionString)
+        {
+      
+            CloudStorageAccount account = CloudStorageAccount.Parse(connectionString);
+            CloudQueueClient client = account.CreateCloudQueueClient();
+            return client;
         }
     }
 }
